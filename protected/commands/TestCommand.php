30e0886546caa68740a3d3b5db440d569f68c025
alexsh <alexeii.shvedov@gmail.com>
2013-09-06 16:43:47 +0700

    stop

--- protected/commands/TestCommand.php
+++ protected/commands/TestCommand.php
@@ -13,8 +13,8 @@ class TestCommand extends CConsoleCommand
 		Yii::import('ext.googleApi.Google_Client');
 		Yii::import('ext.googleApi.contrib.Google_CalendarService');
 		$CLIENT_ID = '508082782199.apps.googleusercontent.com';
+		$clientSecret = '3aINSX9TfUmldjHwmnr9tRH7';
 		$SERVICE_ACCOUNT_NAME = '508082782199@developer.gserviceaccount.com';
-//		$SERVICE_ACCOUNT_NAME = 'alexeii.shvedov@gmail.com';
 
 		// Make sure you keep your key.p12 file in a secure location, and isn't
 		// readable by others.
@@ -23,16 +23,7 @@ class TestCommand extends CConsoleCommand
 		// Load the key in PKCS 12 format (you need to download this from the
 		// Google API Console when the service account was created.
 		$client = new Google_Client();
-		$key = file_get_contents($KEY_FILE);
-//		print_r($key);
-
 		$client->setClientId($CLIENT_ID);
-//		$client->setAssertionCredentials(new Google_AssertionCredentials(
-//				$SERVICE_ACCOUNT_NAME,
-//				array('https://www.googleapis.com/auth/prediction'),
-//				$key)
-//		);
-//		$client->authenticate();
 
 
 		$client->setApplicationName("Google Prediction Sample");
@@ -41,20 +32,32 @@ class TestCommand extends CConsoleCommand
 			$client->setAccessToken($token);
 		}
 
+//		$prn = 'b5tsk41pl49a9mpjfm6c8hqgc8@group.calendar.google.com'; /* replace with your real calendar name */;
+
 		// Load the key in PKCS 12 format (you need to download this from the
 		// Google API Console when the service account was created.
 		$key = file_get_contents($KEY_FILE);
 		$client->setAssertionCredentials(new Google_AssertionCredentials(
 				$SERVICE_ACCOUNT_NAME,
 				array('https://www.googleapis.com/auth/calendar'),
-				$key)
+				$key
+//				,
+//				'notasecret',
+//				'http://oauth.net/grant_type/jwt/1.0/bearer',
+//				$prn
+			)
 		);
 
 		$client->setClientId($CLIENT_ID);
+		$client->setClientSecret($clientSecret);
 		$service = new Google_CalendarService($client);
 
-		$calList = $service->calendarList->listCalendarList();
+		$calList = $service->calendarList->listCalendarList(array('maxResults'=>20, 'showHidden'=>true, 'minAccessRole'=>'writer'));
 		print_r($calList);
+		echo "\n\n";
+
+//		$calendar = $service->calendarList->get('b5tsk41pl49a9mpjfm6c8hqgc8@group.calendar.google.com');
+//		print_r($calendar);
 
 
 		// We're not done yet. Remember to update the cached access token.
