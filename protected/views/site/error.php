16ca3ae988d0b3918ceabb5762090e96f06a66a9
john <evgeniy@solodukhin.ru>
2013-10-18 16:10:58 +0700

    переверстал страницу ошибок

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -7,9 +7,14 @@ $this->breadcrumbs=array(
 	'Error',
 );
 ?>
-<div class="row">
-	<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12">
-		<img class="img-responsive error" src="/images/error.png">
-		<h1 class="error">Документ не найден...<a href="/"><i>На главную</i> &rarr;</a></span></h1>
+<div id="wrap" class="grid ">
+	<div class="flow">
+		<div class="col-12 text-center">
+			<div class="error-number ">
+				<img class="image-responseble" src="/images/error.png" alt=""/>
+				<span>404</span>
+			</div>
+			<h1 class="error">Документ не найден...<a href="/"><i>На главную</i> &rarr;</a></span></h1>
+		</div>
 	</div>
 </div>
